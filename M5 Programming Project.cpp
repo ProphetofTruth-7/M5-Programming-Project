93f1c399ed42df8a011887d33ead33260937fe88
Malakye Brown <prophetoftruth07@gmail.com>
2026-04-23 09:31:33 -0400

    Initial creation of Employee Class. I want to start off in the main, because Inheritance is annoying and I want to be sure of my abilities first

--- M5 Programming Project.cpp
+++ M5 Programming Project.cpp
@@ -1,1 +1,50 @@
-// This is the Main Branch. Delve into the sub-branches for grading
+// This is the Factory Workers Subproject
+#include <iostream>
+using namespace std;
+
+class Employee {
+private:
+	string name;
+	int id;
+	string hireDate;
+public:
+	Employee(string n, int i, string h) : name(n), id(i), hireDate(h) {}
+
+	string getName() {
+		return name;
+	}
+	string getHireDate() {
+		return hireDate;
+	}
+	int getID() {
+		return id;
+	}
+
+	void setName(string x) {
+		name = x;
+	}
+	void setHireDate(string x) {
+		hireDate = x;
+	}
+	void setID(int x) {
+		id = x;
+	}
+
+
+	void printInfo() {
+		cout << "Employee Name: " << name << endl;
+		cout << "Employee ID: " << id << endl;
+		cout << "Employee Hire Date: " << hireDate << endl;
+	}
+};
+
+
+
+int main() {
+	cout << "Welcome to the Factory Workers Subproject!" << endl;
+
+	Employee emp("John Madden", 348392, "04/02/2007");
+	emp.printInfo();
+	
+	return 0;
+}
