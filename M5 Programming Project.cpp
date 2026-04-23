bd1359f54b5a8f549bade29b11ef905c6ea4fc7d
Malakye Brown <prophetoftruth07@gmail.com>
2026-04-23 16:43:55 -0400

    Annoying glitch that breaks the QOL Visual Studio has, so checkpoint. Made the basic BasicShape class

--- M5 Programming Project.cpp
+++ M5 Programming Project.cpp
@@ -1,7 +1,29 @@
 // This is the Basic Shapes Subbranch!
 using namespace std;
+#include <string>
 
-class BasicShape {};
+class BasicShape {
+private:
+	double area;
+	string name;
+public:
+	BasicShape(string n, double area) : name(n), area(a) {}
+
+	double getArea() {
+		return area;
+	}
+	string getName() {
+		return name;
+	}
+
+	void setArea(double a) {
+		area = a;
+	}
+	void setName(string n) {
+		name = n;
+	}
+
+};
 
 
 
