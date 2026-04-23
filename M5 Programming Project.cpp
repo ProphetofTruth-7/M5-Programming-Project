417707ddcbfabe7fbc8dcc6745e2defd4940d360
Malakye Brown <prophetoftruth07@gmail.com>
2026-04-23 15:13:20 -0400

    TeamLeader and Supervisior Classes created

--- M5 Programming Project.cpp
+++ M5 Programming Project.cpp
@@ -43,7 +43,7 @@ private:
 	int shift;
 	double pay;
 public:
-	ProductionWorker(int s, double p, string n, int i, string h) : Employee(n, i, h), shift(s), pay(p) {}
+	ProductionWorker(string n, int i, string h, int s, double p) : Employee(n, i, h), shift(s), pay(p) {}
 
 	int getShift() {
 		return shift;
@@ -55,7 +55,7 @@ public:
 	void setShift(int x) {
 		shift = x;
 	}
-	double getPay(double x) {
+	double setPay(double x) {
 		pay = x;
 	}
 
@@ -66,13 +66,77 @@ public:
 	}
 };
 
+class ShiftSupervisor : public Employee {
+private:
+	double salary;
+	double bonus;
+public:
+	ShiftSupervisor(string n, int i, string h, double s, double b) : Employee(n, i, h), salary(s), bonus(b) {}
+
+	double getSalary() {
+		return salary;
+	}
+	double getBonus() {
+		return bonus;
+	}
+
+	void setSalary(double x) {
+		salary = x;
+	}
+	double setBonus(double x) {
+		bonus = x;
+	}
+
+	void printSupervisorInfo() {
+		printInfo();
+		cout << "Salary: $" << getSalary() << endl;
+		cout << "Bonus: $" << getBonus() << endl;
+	}
+};
+
+class TeamLeader : public ProductionWorker {
+private:
+	double monthlyBonus;
+	double reqTraining;
+	double actualTraining;
+public:
+	TeamLeader(string n, int i, string h, int s, double p, double b, double rt, double at) : ProductionWorker(n, i, h, s, p), monthlyBonus(b), reqTraining(rt), actualTraining(at) {}
+
+	double getMonthlyBonus() {
+		return monthlyBonus;
+	}
+	double getRequiredTraining() {
+		return reqTraining;
+	}
+	double getActualTraining() {
+		return actualTraining;
+	}
+
+	void setMonthlyBonus(double x) {
+		monthlyBonus = x;
+	}
+	double setRequiredTraining(double x) {
+		reqTraining = x;
+	}
+	double setActualTraining(double x) {
+		actualTraining = x;
+	}
+
+	void printLeaderInfo() {
+		printProductionInfo();
+		cout << "Monthly Bonus: $" << getMonthlyBonus() << endl;
+		cout << "Required Training Hours: " << getRequiredTraining() << endl;
+		cout << "Training Hours Attended: " << getActualTraining() << endl;
+	}
+};
+
 
 
 int main() {
 	cout << "Welcome to the Factory Workers Subproject!" << endl;
 
-	ProductionWorker emp(1, 20.35, "John Madden", 348392, "04/02/2007");
-	emp.printProductionInfo();
+	TeamLeader emp("John Madden", 348392, "04/02/2007", 1, 24, 50, 20, 18);
+	emp.printLeaderInfo();
 	
 	return 0;
 }
